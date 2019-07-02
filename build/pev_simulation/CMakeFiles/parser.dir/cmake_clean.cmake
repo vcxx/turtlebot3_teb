9c93bfbfe723eba8cdbd7d49457f40b7ca3c0e3e
vcxx <vincy.vcx@gmail.com>
2019-07-02 01:11:39 -0400

    add pev_simulation pkg for new_pev urdf

---
+++ build/pev_simulation/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/src/parser.cpp.o"
+  "/home/vincy/vin_ws/devel/lib/pev_simulation/parser.pdb"
+  "/home/vincy/vin_ws/devel/lib/pev_simulation/parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/pev_simulation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vincy/vin_ws/src/pev_simulation
+# Build directory: /home/vincy/vin_ws/build/pev_simulation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
